e93aa5de21fddb98142dffad97764e9d8646a6d6
Chancelle2018 <82391458+Chancelle2018@users.noreply.github.com>
2023-02-16 23:47:50 +0100

    Add files via upload

---
+++ ChangeAlpha.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAlpha : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider mainSlider;
+    public GameObject currentGameObject;
+    private Material currentMat;
+    private float alpha;
+
+    // Update is called once per frame
+    
+    void Start()
+    {
+        currentGameObject = gameObject;
+        currentMat = currentGameObject.GetComponent<Renderer>().material;
+        mainSlider.value = currentMat.color.a;
+    }
+
+    public void UpdateAlpha()
+    {
+        Color oldColor = currentMat.color;
+        alpha = mainSlider.value;
+        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, alpha);
+        currentMat.SetColor("_Color", newColor);
+    }
+}
---
+++ MoreCubes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreCubes : MonoBehaviour
+{
+    public GameObject CubePrefabs;
+  
+    // Update is called once per frame
+
+    public void Cube()
+
+    {
+        Instantiate(CubePrefabs);
+        
+    }
+}
